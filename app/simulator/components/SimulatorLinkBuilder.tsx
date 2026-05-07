ce16a8e284af32f59c7185435e61e09f11abe62a
신원철 <sin100007@gmail.com>
2026-05-08 02:02:02 +0900

    djiodjiodd:

--- app/simulator/components/SimulatorLinkBuilder.tsx
+++ app/simulator/components/SimulatorLinkBuilder.tsx
@@ -84,7 +84,7 @@ const DEFAULT_MASK_ZONES: MaskZoneDefinition[] = [
 ];
 
 const CUSTOMER_SHARE_MESSAGE =
-  "대표적인 공간 이미지에 500여가지 필름을 적용해, 조합에 따른 뉘앙스를 보여드립니다.\n*Chrome(크롬브라우저)에 최적화되어 있습니다.";
+  "시뮬봇\n대표적인 공간 이미지에 500여가지 필름을 적용해, 조합에 따른 뉘앙스를 보여드립니다.\n*Chrome(크롬브라우저)에 최적화되어 있습니다.";
 
 const COLORS = {
   bg: "#05023B",
